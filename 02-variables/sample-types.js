b4f9c71ddf27900a2cea57057ed636f98634c9ce
Maxiflexy <onyemax247@gmail.com>
2024-07-07 12:28:39 +0100

    data variables

---
+++ 02-variables/sample-types.js
@@ -0,0 +1,7 @@
+var found = true;
+var grade = 88.6;
+var firstName = "Maxi";
+var lastName = 'Ejemba';
+console.log(found);
+console.log("THe grade is " + grade);
+console.log("Hi ".concat(firstName, " ").concat(lastName));
---
+++ 02-variables/sample-types.ts
@@ -0,0 +1,10 @@
+let found : boolean = true;
+let grade : number = 88.6;
+let firstName : string = "Maxi";
+let lastName : string = 'Ejemba';
+
+
+
+console.log(found);
+console.log("THe grade is " + grade);
+console.log(`Hi ${firstName} ${lastName}`);
