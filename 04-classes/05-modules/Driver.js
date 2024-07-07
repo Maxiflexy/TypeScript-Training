02512d19907317d1cc439cabada6126cc466ca5d
Maxiflexy <onyemax247@gmail.com>
2024-07-07 15:53:30 +0100

    modules

---
+++ 04-classes/05-modules/Driver.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Customer_1 = require("./Customer");
+// create an instance of customer
+var myCustomer = new Customer_1.Customer("Martin", "Onyeka");
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
---
+++ 04-classes/05-modules/Driver.ts
@@ -0,0 +1,7 @@
+import {Customer} from "./Customer";
+
+// create an instance of customer
+let myCustomer = new Customer("Martin", "Onyeka");
+
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
