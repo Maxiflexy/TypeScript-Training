b10078124edfb15738656793f8b2e91c49295059
Maxiflexy <onyemax247@gmail.com>
2024-07-07 15:11:46 +0100

    create classes

---
+++ 04-classes/01-class-definition/Customer.js
@@ -0,0 +1,11 @@
+var Customer = /** @class */ (function () {
+    function Customer() {
+    }
+    return Customer;
+}());
+// create an instance of customer
+var myCustomer = new Customer();
+myCustomer.firstName = "Martin";
+myCustomer.lastName = "Onyeka";
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
---
+++ 04-classes/01-class-definition/Customer.ts
@@ -0,0 +1,17 @@
+class Customer{
+
+    firstName: string;
+    lastName: string;
+
+    constructor(theFirst: string, theLast: string){
+        this.firstName = theFirst;
+        this.lastName = theLast;
+    }
+
+}
+
+// create an instance of customer
+let myCustomer = new Customer("Martin", "Onyeka");
+
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
